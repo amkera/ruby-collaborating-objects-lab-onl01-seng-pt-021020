964a0e6d309fe18bb2ac45b7ed32c0cecaf59c68
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2021-02-25 00:34:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist #name= sets the song name 
   #artist= sets the artist object to belong to the song 
@@ -22,12 +23,14 @@ class Song
     songs = title.split(" - ")[1]
     artist = title.split(" - ")[0]
     song = self.new(songs)
-    song.artist_name = artist
+    song.artist_name = artist #if you don't call artist_name method, 
     song
   end
 
-  def artist_name=(name)
+  def artist_name=(name) #setting artist name attribute
     self.artist = Artist.find_or_create_by_name(name)
-    self.artist.add_song(self)
+    self.artist.add_song(self) 
+    #self is an instance of song, self.artist returns an instance of Artist class, and then add_song is telling an instance of Artist to add a song to itself 
+    binding.pry
   end
 end
